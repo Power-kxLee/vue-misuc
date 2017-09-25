9811cc857fa642dd58fd36dce218f043f2331b0a
lee <578242325@qq.com>
2017-09-25 18:04:42 +0800

    mintui

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <mt-header fixed title="固定在顶部"></mt-header>
     <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from 'vue';
 import App from './App';
 import router from './router';
-
+import { Header } from 'mint-ui';
+Vue.component(Header.name, Header);
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
 new Vue({
     el: '#app',
     router,
