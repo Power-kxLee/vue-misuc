e29923e57cfe100e98d11062623c80b19369d542
lee <578242325@qq.com>
2017-09-27 22:59:25 +0800

    '完成demo页面'

--- src/components/music.vue
+++ src/components/music.vue
@@ -3,9 +3,18 @@
         <div class="open-music-content">
             <div class="m-song-disc">
                 <div class="m-song-turn">
-                    
+                    <div class="m-song-img">
+                        <img src="../assets/images/109951163024374278.jpg">
+                    </div>
                 </div>
             </div>
+            <article class="m-song-info">
+                <header class="m-song-header">
+                    <span class="m-song-sname">To Know You</span>
+                    <span class="m-song-gap">-</span>
+                    <span class="m-song-autr">Do As Infinity</span>
+                </header>
+            </article>
         </div>
     </div>
 </template>
@@ -27,9 +36,6 @@
         bottom: 0;
     }
     .music-main{
-        position: absolute;
-        top:0px;
-        left: 0px;
         width: 100%;
         height: 100%;
         min-width: 320px;
@@ -37,9 +43,7 @@
         .open-music-content{
             background-position: 50%;
             background-repeat: no-repeat;
-            background-size: auto 100%;
-            -webkit-transform: scale(1.5);
-            transform: scale(1.5);
+            background-size: auto 100%; 
             -webkit-transform-origin: center top;
             transform-origin: center top;
             z-index: -1;
@@ -50,14 +54,70 @@
                 width: 300px;
                 height: 300px;
                 position: relative;
+                margin: 0 auto;
+                margin-top: 70px;
+                &:after{
+                    content:"";
+                    position: absolute;
+                    width: 96px;
+                    height: 137px;
+                    top: -70px;
+                    left: 133px;
+                    background-image:url(../assets/images/needle.png);
+                    background-size: contain;
+                    z-index: 3;
+                }
                 .m-song-turn{
                     width: 100%;
                     height: 100%;
                     &:after{
                         @include pseudo();
+                        background-image: url(../assets/images/disc.png);
+                        background-size: contain;
+                        z-index: 2;
+                    }
+                    .m-song-img{
+                        position: absolute;
+                        top:50%;
+                        left: 50%;
+                        margin-left: -92px;
+                        margin-top: -92px;
+                        width: 184px;
+                        height: 184px;
+                        border-radius: 50%;
+                        overflow: hidden;              
+                        background-image: url(../assets/images/disc_default.png);
+                        transform: translate(-50%,-50%);
+                        transform-origin:50% 50%;
+                        animation: circling 20s infinite linear;
+                        
+                        img{
+                            width: 100%;
+                            vertical-align: middle;
+                        }
                     }
                 }
             }
         }
     }
+    .m-song-info{
+        padding: 0 35px;
+        margin-top: 25px;
+        .m-song-header{
+            font-size: 18px;
+            color: white;
+            text-align: center;
+        }
+    }
+
+    @keyframes circling {
+        0% {
+            transform: rotate(0deg)
+        }
+
+        to {
+            transform: rotate(1turn)
+        }
+    }
+
 </style>
